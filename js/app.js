e4b733ee6a35b8b346c0b709ff118b3e05e65102
paullikestocode <paul_schrage@yahoo.com>
2014-10-01 10:38:03 -0700

    Not going to finish this exercise

--- js/app.js
+++ js/app.js
@@ -1,6 +1,71 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url:'/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl',
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+	$urlRouterProvider.otherwise('home');
+}])
+
+.factory('posts', [function() {
+	var o = {
+		posts: [
+		{title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 2},		
+		{title: 'post 3', upvotes: 15},
+		{title: 'post 4', upvotes: 9},
+		{title: 'post 5', upvotes: 4}
+		]
+	};
+	return o;
+}])
+
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-	$scope.test = 'Hello world!';
+'posts',
+function($scope, posts){
+	$scope.posts = posts.posts;
+	$scope.addPost = function() {
+		if($scope.title === '') {return; }
+		$scope.posts.push({
+			title: $scope.title, 
+			link: $scope.link,
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+			]
+		});
+		$scope.title = '';
+		$scope.link = '';
+	};
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	};
+}])
+
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+	$scope.post = posts.posts[$stateParams.id];
 }]);
+
+// $scope.posts = [
+// 	'post 1',
+// 	'post 2',
+// 	'post 3',
+// 	'post 4',
+// 	'post 5'
+// ];
